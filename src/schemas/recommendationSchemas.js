5e7ba411b61cb3506fb7385671c18b9351b0065c
LNakandakare <lucasnakaan@gmail.com>
2021-03-15 13:33:11 -0300

    refactor: new recommendation schema

--- src/schemas/recommendationSchemas.js
+++ src/schemas/recommendationSchemas.js
@@ -2,7 +2,7 @@ const joi = require('joi');
 
 const newRecommend = joi.object({
     name: joi.string().required(), 
-    genresIds: joi.array().required(), 
+    genresIds: joi.array().items(joi.number().min(1)).required(), 
     youtubeLink: joi.string().uri().required()
 });
 
